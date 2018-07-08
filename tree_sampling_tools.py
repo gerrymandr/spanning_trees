0a5582ef81be2799ecfba13540a100284e5f1a66
Lorenzo Najt <lnajt4@gmail.com>
2018-07-08 04:27:22 -0400

    branch length
    I changed it to count the branch length as it builds it -- not sure if this was slowing it down or not.

--- tree_sampling_tools.py
+++ tree_sampling_tools.py
@@ -47,15 +47,19 @@ def random_spanning_tree_wilson(graph):
     #The David Wilson random spanning tree algorithm
     tree_edges = []
     hitting_set = set ( [ random.choice(list(graph.nodes()))])
+    allowable_set = set(graph.nodes())
     while len(hitting_set) < len(graph):
-        allowable_set = list(set(graph.nodes()).difference(hitting_set))
-        start_node = random.choice(allowable_set)
+        #allowable_set = list(set(graph.nodes()).difference(hitting_set))
+        #If we can handle the step of choosing the allowable set more 
+        #efficiently, we can speed stuff up... this is the bottle neck
+        start_node = random.choice(list(allowable_set))
         trip = random_walk_until_hit(graph, start_node, hitting_set)
-        new_branch = loop_erasure(trip)
-        for i in range(len(new_branch) - 1):
+        new_branch, branch_length = loop_erasure(trip)
+        for i in range(branch_length - 1):
             tree_edges.append( [ new_branch[i], new_branch[i + 1]])
-        for v in new_branch:
+        for v in new_branch[:-1]:
             hitting_set.add(v)
+            allowable_set.remove(v)
     tree = nx.DiGraph()
     tree.add_nodes_from(list(graph.nodes()))
     tree.add_edges_from(tree_edges)
@@ -84,15 +88,15 @@ def loop_erasure(trip):
     loop_erased_walk_indices = []
     last_occurance = n - trip[::-1].index(trip[0]) - 1
     loop_erased_walk_indices.append(last_occurance)
-        
+    branch_length = 0
     while trip[loop_erased_walk_indices[-1]] != trip[-1]:
         last_occurance = n -  trip[::-1].index(trip[loop_erased_walk_indices[-1]])  -1
         loop_erased_walk_indices.append(last_occurance + 1)
-        
+        branch_length += 1
     loop_erased_trip = [trip[i] for i in loop_erased_walk_indices]
     
-    return loop_erased_trip
-    
+    return (loop_erased_trip, branch_length + 1)
+    #I don't think that passing the length sped it up at all...
 
 #################3
     
