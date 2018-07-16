07aaef4c456c616e72b61bf827f3339b7ca97db3
Lorenzo Najt <lnajt4@gmail.com>
2018-07-16 12:27:08 -0400

    added classes file

---
+++ classes.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 16 12:21:16 2018
+
+@author: MGGG
+"""
+
+'''
+The sampler class:
+    
+    variables: graph, num_partitions, num_blocks, list of trees
+    
+    functions: random_sample_wilson (import from tree sample)
+     divide and consquer
+     
+    Walk tools ... takes a tree in the list of trees and walks until it meets the criterion... clean up walk tools, and import from there...
+    
+    
+    projections (import from projection tools)
+    
+The tree class: 
+    extends graph
+    
+    variables: root, topological_ordering'''
